6d5d923ba23b54911829807aaa638e53c466feb8
Oscar Coppola <copp.oscar0213@gmail.com>
2024-11-23 17:25:00 -0500

    --yes --

--- monster-main/build.sh
+++ monster-main/build.sh
@@ -4,4 +4,4 @@
 sudo apt install opam
 opam init
 opam install "forester<=3.1.0"
-opam exec --yes forester build --dev --root mon-0001 trees/
+opam exec --yes -- forester build --dev --root mon-0001 trees/
